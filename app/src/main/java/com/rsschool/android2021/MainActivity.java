41c89fff426f6102dad8a317b9435fb237e4142e
Karina Osoprilko <71908284+chamomilek@users.noreply.github.com>
2021-05-31 23:15:41 +0300

    Update MainActivity.java

--- app/src/main/java/com/rsschool/android2021/MainActivity.java
+++ app/src/main/java/com/rsschool/android2021/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,10 +20,25 @@ public class MainActivity extends AppCompatActivity {
         final Fragment firstFragment = FirstFragment.newInstance(previousNumber);
         final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, firstFragment);
-        // TODO: invoke function which apply changes of the transaction
+        transaction.commit(); //? TODO: invoke function which apply changes of the transaction
     }
 
+    //запуск фрагмета2 с передачей min и max значения генерируемого числа
     private void openSecondFragment(int min, int max) {
-        // TODO: implement it
+        final Fragment secondFragment = SecondFragment.newInstance(min,max);
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, secondFragment);
+        transaction.commit();
+    }
+    // реализация метода интерфейса ActionPerformedListener для вызова второго фрагмента2
+    @Override
+    public void actionPerformed2(int min, int max) {
+        openSecondFragment(min, max);
+    }
+
+    // реализация метода интерфейса ActionPerformedListener для вызова второго фрагмента1
+    @Override
+    public void actionPerformed1(int number) {
+        openFirstFragment(number);
     }
 }
